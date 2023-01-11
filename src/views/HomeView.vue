123e7e154775e342f41487a4831ad6a96fb0b861
Ermiyas Zeleke <78529847+ermi2021@users.noreply.github.com>
2023-01-11 13:28:05 +0300

    completed making feedback on choice clicks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,172 +1,107 @@
 <template>
   <main class="flex h-screen items-center justify-center bg-gray-100">
-
     <!-- quiz container -->
-    <div class="overflow-hidden bg-white flex-none container relative shadow-lg rounded-lg px-12 py-6">
-       <img src="../assets/images/abstract.svg" alt="" class="absolute -top-10 left-0 object-none">
+    <div
+      class="
+        overflow-hidden
+        bg-white
+        flex-none
+        container
+        relative
+        shadow-lg
+        rounded-lg
+        px-12
+        py-6
+      "
+    >
+      <img
+        src="../assets/images/abstract.svg"
+        alt=""
+        class="absolute -top-10 left-0 object-none"
+      />
 
-       <!-- contents -->
-       <div class="relative z-20">
-
+      <!-- contents -->
+      <div class="relative z-20">
         <div class="text-right text-gray-800 mt-4">
-         <p class="text-sm leading-1">Score</p>
-         <p class="font-bold">60</p>
-      </div>
-
-     <!-- timer container -->
-      <div class="bg-white shadow-lg p-2 rounded-full w-full h-5">
-       <div class="bg-blue-700 rounded-full w-11/12 h-full"></div>
-      </div>
-
-
-      <!-- question container -->
-      <div
-        class="
-          rounded-lg
-          neumorph-1
-          bg-grey-100
-          p-2
-          text-center
-          font-bold
-          text-gray-800
-          mt-8
-        "
-      >
-        <div class="bg-white p-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit?
+          <p class="text-sm leading-1">Score</p>
+          <p class="font-bold">60</p>
         </div>
-      </div>
- 
-      <!-- options container -->
-      <div>
-      
-      
-      
-        <!-- option container -->
-        <div class="option-default bg-gray-100 p-2 neumorph-1 rounded-lg mb-3 relative">
-          <div
-            class="
-              bg-blue-500
-              p-1
-              transform
-              rotate-45
-              rounded-md
-              w-10
-              h-10
-              text-white
-              font-bold
-              absolute
-              right-0
-              top-0
-              shadow-md
-            "
-          >
-            <p class="transform -rotate-45">+10</p>
-          </div>
-          <div class=" rounded-lg font-bold flex p-2">
-            <!-- option id -->
-            <div class="bg-gray-400 p-3 rounded-lg">C</div>
-            <div class="flex items-center pl-6">Thor</div>
-          </div>
+
+        <!-- timer container -->
+        <div class="bg-white shadow-lg p-2 rounded-full w-full h-5">
+          <div class="bg-blue-700 rounded-full w-11/12 h-full"></div>
         </div>
-        
-        <!-- option container -->
-        <div class="option-wrong bg-gray-100  p-2 neumorph-1 rounded-lg mb-3 relative">
-          <div
-            class="
-              bg-blue-500
-              p-1
-              transform
-              rotate-45
-              rounded-md
-              w-10
-              h-10
-              text-white
-              font-bold
-              absolute
-              right-0
-              top-0
-              shadow-md
-            "
-          >
-            <p class="transform -rotate-45">+10</p>
-          </div>
-          <div class=" rounded-lg font-bold flex p-2">
-            <!-- option id -->
-            <div class="bg-gray-400 p-3 rounded-lg">C</div>
-            <div class="flex items-center pl-6">Thor</div>
+
+        <!-- question container -->
+        <div
+          class="
+            rounded-lg
+            neumorph-1
+            bg-grey-100
+            p-2
+            text-center
+            font-bold
+            text-gray-800
+            mt-8
+          "
+        >
+          <div class="bg-white p-5">
+            {{ currentQuestion.question }}
           </div>
         </div>
 
-         <!-- option container -->
-         <div class="bg-gray-100 option-correct p-2 neumorph-1 rounded-lg mb-3 relative">
-          <div
-            class="
-              bg-blue-500
-              p-1
-              transform
-              rotate-45
-              rounded-md
-              w-10
-              h-10
-              text-white
-              font-bold
-              absolute
-              right-0
-              top-0
-              shadow-md
-            "
-          >
-            <p class="transform -rotate-45">+10</p>
-          </div>
-          <div class=" rounded-lg font-bold flex p-2">
-            <!-- option id -->
-            <div class="bg-gray-400 p-3 rounded-lg">C</div>
-            <div class="flex items-center pl-6">Thor</div>
-          </div>
-        </div>
-
-         <!-- option container -->
-         <div class="bg-gray-100 option-default p-2 neumorph-1 rounded-lg mb-3 relative">
-          <div
-            class="
-              bg-blue-500
-              p-1
-              transform
-              rotate-45
-              rounded-md
-              w-10
-              h-10
-              text-white
-              font-bold
-              absolute
-              right-0
-              top-0
-              shadow-md
-            "
-          >
-            <p class="transform -rotate-45">+10</p>
-          </div>
-          <div class=" rounded-lg font-bold flex p-2">
-            <!-- option id -->
-            <div class="bg-gray-400 p-3 rounded-lg">C</div>
-            <div class="flex items-center pl-6">Thor</div>
+        <!-- options container -->
+        <div>
+          <!-- option container -->
+          <div v-for="(choice, item) in currentQuestion.choices" :key="item">
+            <div
+              class="
+                option-default
+                bg-gray-100
+                p-2
+                neumorph-1
+                rounded-lg
+                mb-3
+                relative
+              "
+              :ref="optionChosen"
+              @click="onOptionClicked(choice, item)"
+            >
+              <div
+                class="
+                  bg-blue-500
+                  p-1
+                  transform
+                  rotate-45
+                  rounded-md
+                  w-10
+                  h-10
+                  text-white
+                  font-bold
+                  absolute
+                  right-0
+                  top-0
+                  shadow-md
+                "
+              >
+                <p class="transform -rotate-45">+10</p>
+              </div>
+              <div class="rounded-lg font-bold flex p-2">
+                <!-- option id -->
+                <div class="bg-gray-400 p-3 rounded-lg">{{item}}</div>
+                <div class="flex items-center pl-6">{{choice}}</div>
+              </div>
+            </div>
           </div>
         </div>
-      
-       
+
+        <!-- progress indicator container-->
+        <div class="mt-8 text-center">
+          <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
+          <p class="font-bold text-gray-800">2/10</p>
+        </div>
       </div>
-
-      <!-- progress indicator container-->
-      <div class="mt-8 text-center">
-        <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
-        <p class="font-bold text-gray-800">2/10</p>
-      </div>
-
-
-       </div>
-     <!-- score container -->
-     
+      <!-- score container -->
     </div>
   </main>
 </template>
@@ -174,43 +109,86 @@
 .neumorph-1 {
   box-shadow: 6px 6px 18px rgba(0, 0, 0, 0.09), -6px -6px 18px #ffffff;
 }
-.container{
+.container {
   max-width: 400px;
   border-radius: 25px;
 }
 </style>
 
 <script>
-import {ref} from 'vue'
-  export default {
-    setup(){
-      const currentQuestion = ref({
-        question:'',
-        answer:1,
-        choices:[]
-      })
+import { onMounted, ref } from "vue";
+export default {
+  setup() {
 
-      const questions=[{
-          question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
-          answer:1,
-          choices:["Option 1", "Option 2", "Option 3", "Option 4"]
+   
+    let questionCounter = ref(0);
+
+    const currentQuestion = ref({
+      question: "",
+      answer: 1,
+      choices: [],
+    });
+
+    const questions = [
+      {
+        question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: 1,
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
       },
       {
-          question: "Question 2 Lorem ipsum dolor sit amet consectetur adipisicing elit?",
-          answer:3,
-          choices:["Option 1", "Option 2", "Option 3", "Option 4"]
+        question:
+          "Question 2 Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: 3,
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
       },
       {
-          question: "Question 3 Lorem ipsum dolor sit amet consectetur adipisicing elit?",
-          answer:4,
-          choices:["Option 1", "Option 2", "Option 3", "Option 4"]
+        question:
+          "Question 3 Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: 4,
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
       },
       {
-          question: "Question 4 Lorem ipsum dolor sit amet consectetur adipisicing elit?",
-          answer:2,
-          choices:["Option 1", "Option 2", "Option 3", "Option 4"]
+        question:
+          "Question 4 Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: 2,
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+    ];
+
+    const onQuizStart = () => {
+      currentQuestion.value = questions[questionCounter.value];
+    };
+
+    //methods
+    let itemRef = [];
+    const optionChosen = (element) =>{
+      if(element){
+        itemRef.push(element);
       }
-    ]
     }
-  }
+
+    const onOptionClicked = (choice, item) => {
+      const divContainer = itemRef[item];
+      const optionId = item++;
+
+      if(currentQuestion.value.answer == optionId) {
+        console.log("You are correct!")
+        divContainer.classList.add('option-correct');
+        divContainer.classList.remove('option-default');
+      }else{
+        divContainer.classList.add('option-wrong');
+        divContainer.classList.remove('option-default');
+      }
+     
+    };
+
+    //lifecycle hooks
+    onMounted(() => {
+      onQuizStart();
+    });
+
+    // return
+    return { currentQuestion, questions, questionCounter, onQuizStart, onOptionClicked , optionChosen};
+  },
+};
 </script>
